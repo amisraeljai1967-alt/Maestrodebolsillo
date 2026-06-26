13d8bfdbdf9c5d13d3fed78eef1766fa5302e386
amisraeljai1967-alt <amisraeljai1967@gmail.com>
2026-06-25 20:54:54 -0600

    Update verificar.js

--- netlify/functions/verificar.js
+++ netlify/functions/verificar.js
@@ -1,16 +1,25 @@
 exports.handler = async function(event, context) {
   const cookies = event.headers.cookie || '';
   const tieneAcceso = cookies.includes('acceso=SI');
-  
+  const page = event.queryStringParameters.page;
+
   if (tieneAcceso) {
+    const fs = require('fs');
+    const path = require('path');
+    const filePath = path.join(__dirname, '../../', page + '.html');
+    const html = fs.readFileSync(filePath, 'utf8');
     return {
       statusCode: 200,
-      body: JSON.stringify({ acceso: true })
+      headers: { 'Content-Type': 'text/html' },
+      body: html
     };
   }
-  
+
   return {
-    statusCode: 401,
-    body: JSON.stringify({ acceso: false })
+    statusCode: 302,
+    headers: {
+      'Location': 'https://pay.hotmart.com/X106434037V'
+    },
+    body: ''
   };
 };
